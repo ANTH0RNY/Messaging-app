2012af2e54c84f76019329e6e2ea1f9d6553f38e
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2025-06-19 19:56:43 +0300

    syncing changes

--- messaging_frontend/src/components/ChatArea.vue
+++ messaging_frontend/src/components/ChatArea.vue
@@ -1,27 +1,17 @@
 <template>
   <div class="h-full flex flex-col gap-2 dark:text-white bg-amber-100 dark:bg-slate-900">
-    <div class="header flex gap-1 px-2 items-center justify-evenly">
+    <div class="header flex gap-1 px-2 py-4 items-center justify-evenly">
       <!-- <h2 class="border border-black flex items-end px-0.5 font-bold text-lg py-0">Charts</h2> -->
       <!-- <MenuIcon1 class="w-8 h-8" /> -->
-      <span class="bg-slate-500 py-1 pe-3 ps-2 my-1 rounded-full items-center justify-center flex">
+      <span class="bg-slate-500 py-1 pe-3 ps-2  rounded-full items-center justify-center flex">
         <SearchIcon class="" />
-        <input
-          type="text"
-          placeholder="Search"
-          class="rounded-full ps-1 w-fit focus:outline-0 focus:border-0"
-        />
+        <input type="text" placeholder="Search" class="rounded-full ps-1 w-fit focus:outline-0 focus:border-0" />
       </span>
     </div>
-    <div class="no-scroll charts flex flex-col gap-2 p-2 overflow-y-scroll">
-      <ChatCard
-        v-for="(item, idx) in chatItems"
-        :key="idx"
-        :profile-photo-url="item.profilePhotoUrl"
-        :contact-name="item.contactName"
-        :last-msg="item.lastMsg"
-        :number-of-unread-msg="item.numberOfUnreadMsg"
-        :last-msg-date="item.lastMsgDate"
-      />
+    <div class="no-scroll charts flex flex-col gap-2 px-2 overflow-y-scroll">
+      <ChatCard v-for="(item, idx) in chatItems" :key="idx" :profile-photo-url="item.profilePhotoUrl"
+        :contact-name="item.contactName" :last-msg="item.lastMsg" :number-of-unread-msg="item.numberOfUnreadMsg"
+        :last-msg-date="item.lastMsgDate" />
     </div>
   </div>
 </template>
