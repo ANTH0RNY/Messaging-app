97fa39f90628b7b8b4d2b11b76d6473fd523657f
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2025-06-19 17:08:15 +0300

    frontend(feat):Adds message area topbar

--- messaging_frontend/src/components/MessageArea.vue
+++ messaging_frontend/src/components/MessageArea.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="overflow-y-scroll no-scroll border border-black dark:bg-gray-900 w-full">
-    <MessageCard v-for="mess in messages" :key="mess.messageId" :message="mess"/>
+  <div class="w-full flex flex-col dark:bg-gray-900 dark:text-white">
+    <MessageNav :profileUrl="profileURL" :lastSeen="lastSeen" :username="username" />
+    <div class="overflow-y-scroll no-scroll border border-black dark:bg-gray-900 w-full">
+      <MessageCard v-for="mess in messages" :key="mess.messageId" :message="mess" />
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
 import MessageCard from './MessageCard.vue';
+import MessageNav from "@components/MessageNav.vue"
 import { messages } from '@/utils/messages';
+const profileURL = "https://images.unsplash.com/photo-1743875929006-803ea54cafc1?q=80&w=1065&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+const username = "Jane Doe"
+const lastSeen = "last seen yesterday"
 </script>
---
+++ messaging_frontend/src/components/MessageNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="flex items-center py-1 px-2 dark:bg-gray-800">
+    <div class="flex items-center gap-2">
+      <div class="flex items-center justify-center w-15 h-15 rounded-full overflow-hidden">
+        <img v-if="profileUrl" :src="profileUrl" :alt="`${username} profile picture`">
+        <ProfileIcon1 v-else class="w-8/12 h-8/12" />
+      </div>
+      <div class="flex flex-col gap-0">
+        <p class="font-bold p-0 text-lg m-0">{{ username }}</p>
+        <p class="font-lighter text-xs m-0 p-0">{{ lastSeen }}</p>
+      </div>
+    </div>
+    <div class="flex items-center ml-auto gap-4">
+      <PhoneIcon1 class="hover:cursor-pointer hover:bg-gray-700 rounded-full p-2 box-content" />
+      <SearchIcon class="hover:cursor-pointer hover:bg-gray-700 rounded-full p-2 box-content" />
+      <MenuDotIcon1 class="hover:cursor-pointer hover:bg-gray-700 rounded-full p-2 box-content" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import PhoneIcon1 from "@icons/PhoneIcon1.vue"
+import SearchIcon from "@icons/SearchIcon.vue"
+import MenuDotIcon1 from "@icons/MenuDotIcon1.vue"
+import ProfileIcon1 from "@icons/ProfileIcon1.vue"
+
+interface PropType {
+  profileUrl?: string
+  username: string,
+  lastSeen: string,
+}
+const { profileUrl, username, lastSeen } = defineProps<PropType>();
+</script>
